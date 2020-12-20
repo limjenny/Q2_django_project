68a730d4fe3885a7ccb622ace0dca70ebb891a6a
limjenny <1j2nny11@naver.com>
2020-12-20 21:01:31 +0900

    Q2_django_project

---
+++ bookmark/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Bookmark(models.Model):
+    site_name = models.CharField(max_length=100)
+    url = models.URLField('Site URL')
+    def __str__(self):
+        return " : "+self.site_name + ", : "+self.url
+
+    def get_absolute_url(self):
+        return reverse('bookmark:detail', args=[str(self.id)])
